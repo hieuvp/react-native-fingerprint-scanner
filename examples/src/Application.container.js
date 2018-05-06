9b258ebc79901c53f6f0c26c2582152e35c5869c
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-05-06 18:22:22 +0700

    chore(examples): update isSensorAvailable usage

--- examples/src/Application.container.js
+++ examples/src/Application.container.js
@@ -15,6 +15,7 @@ class Application extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      biometryType: undefined,
       errorMessage: undefined,
       popupShowed: false
     };
@@ -31,11 +32,12 @@ class Application extends Component {
   componentDidMount() {
     FingerprintScanner
       .isSensorAvailable()
+      .then(biometryType => this.setState({ biometryType }))
       .catch(error => this.setState({ errorMessage: error.message }));
   }
 
   render() {
-    const { errorMessage, popupShowed } = this.state;
+    const { biometryType, errorMessage, popupShowed } = this.state;
 
     return (
       <View style={styles.container}>
@@ -54,6 +56,12 @@ class Application extends Component {
         >
           <Image source={require('./assets/finger_print.png')} />
         </TouchableOpacity>
+
+        {biometryType && (
+          <Text style={styles.biometryType}>
+            Type of biometric authentication: {biometryType}.
+          </Text>
+        )}
 
         {errorMessage && (
           <Text style={styles.errorMessage}>
