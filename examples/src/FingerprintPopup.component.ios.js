27af64f9968d1f422e77ef2e76eaf5aec63b0c8f
Júlio César Mussi <jc.mussi99@gmail.com>
2019-09-10 14:13:10 -0300

    fix(FingerprintPopup.component.ios.js): Fixed deprecated AlertIOS
    Fixed wrong commit message. Commiting from github due to time issues, sorry

--- examples/src/FingerprintPopup.component.ios.js
+++ examples/src/FingerprintPopup.component.ios.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Alert } from 'react-native';
+import {Alert} from 'react-native';
 import FingerprintScanner from 'react-native-fingerprint-scanner';
 
 class FingerprintPopup extends Component {
