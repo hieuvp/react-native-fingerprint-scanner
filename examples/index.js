2cac2124ed9e6dff4053d55cee3424f22049c22f
Valdio Veliu <valdioveliu@gmail.com>
2022-01-11 14:45:08 +0100

    Revert "upgrade react-native on example project"
    This reverts commit 7a165b4075e3bd3543c9768f7ead0a23daf61bbd.

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,4 @@
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import Application from './src/Application.container';
 
 AppRegistry.registerComponent('examples', () => Application);
